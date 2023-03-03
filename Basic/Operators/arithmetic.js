d3dff507b36c333545fe67802207583da24d7d26
WSL <tartaacanon@gmail.com>
2023-03-03 07:13:45 -0500

    Operators: Arithmetics, Comparation & Assignment

---
+++ Basic/Operators/arithmetic.js
@@ -0,0 +1,34 @@
+// addition
+console.log(1 + 8);
+
+// subtraction
+console.log(50 - 35);
+
+// multiplication
+console.log(1 * 20);
+
+// division
+console.log(1 / 5);
+
+// Strings
+console.log("Diego" + " De Granda");
+
+// negation
+console.log(!false);
+
+// assign
+var a = 1;
+
+// comparation
+console.log(4 == "4");
+console.log(2 === "2");
+console.log(5 < 9);
+console.log(5 > 2);
+console.log(5 <= 5);
+console.log(5 >= 4);
+
+// The and of python in js
+a && b 
+
+// The or of python in js
+a || b
