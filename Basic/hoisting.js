596659c03803a4e13d1b990f5614d8d92ff1fb93
WSL <tartaacanon@gmail.com>
2023-03-03 05:35:26 -0500

    Hoisting

---
+++ Basic/hoisting.js
@@ -0,0 +1,18 @@
+// Declaring Variable
+var teacherName;
+
+// Initializing a Variable
+teacherName = "Diego";
+
+// Hoisting 
+console.log(text1)
+var text1 = "hoisting1"
+
+// This is why the first example shows in terminal 'undefined', js make this because the order its important because of the hoisting, we have to make first the variable and then we make the console.log
+var example = undefined 
+console.log(example)
+
+// Right way to show the text with good hoisting
+var text2 = "hoisting2"
+console.log(text2)
+
