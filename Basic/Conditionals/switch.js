cebf26fff2fcbf83115bf59931cf9911b5cb17f7
WSL <tartaacanon@gmail.com>
2023-03-03 08:46:30 -0500

    Conditional: Switch

---
+++ Basic/Conditionals/switch.js
@@ -0,0 +1,16 @@
+number = 34343;
+
+switch (number) {
+    case 1: 
+        console.log("I'm number 1");
+        break; 
+    case 2:
+        console.log("I'm number 2");
+        break;
+    case 3:
+        console.log("I'm number 3");
+        break;
+    default:
+        console.log("Nothing");
+        break;
+}
