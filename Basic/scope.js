17d0053f1fc7591c1454d329d795e637afe4de71
WSL <tartaacanon@gmail.com>
2023-03-03 05:03:23 -0500

    Differences between Scope global and Scope local

---
+++ Basic/scope.js
@@ -0,0 +1,11 @@
+// This is a variable Scope global
+var firstName = "Diego";
+
+// Here is when use the scope global of firstName y the function fullName
+// We cannot use the local variable 'lastName' which is declared within the function 'fullName' outside of its scope.
+function fullName() {
+    var lastName = "De Granda";
+    return firstName + " " + lastName;
+}
+
+console.log(fullName())
