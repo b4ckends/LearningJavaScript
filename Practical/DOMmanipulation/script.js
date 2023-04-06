affd6c6da8fe2f980e1c0ed4eecc29a601652f28
rs3c <rs3c@localhost.localdomain>
2023-04-06 17:55:53 -0600

    Connect JavaScript with HTML

---
+++ Practical/DOMmanipulation/script.js
@@ -0,0 +1,12 @@
+const title = document.querySelector('h1');
+const medium_paragaph = document.querySelector('.medium-paragraph');
+const pid = document.getElementById('pid');
+const input = document.querySelector('input');
+
+
+console.log({
+    title,
+    medium_paragaph,
+    pid,
+    input
+});
