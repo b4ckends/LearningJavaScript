f89f66650dcf3fd7143bbf9313ad077da4762897
rs3c <rs3c@localhost.localdomain>
2023-04-07 12:15:27 -0600

    AddEventListener

---
+++ Practical/DOMmanipulation/addEventListener/script.js
@@ -0,0 +1,15 @@
+const h1 = document.querySelector('h1');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalculate');
+const pResult = document.querySelector('#result');
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', addInputValues());
+
+function addInputValues(event) {
+    console.log({event});
+    event.preventDefault();
+    const addResult = calculation1.value + calculation2.value;
+    pResult.innerText = "Result: " + addResult;
+}
