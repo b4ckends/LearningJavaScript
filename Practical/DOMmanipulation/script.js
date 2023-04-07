47688c033ef0e6bfb58d2bec5ccedd69dbaafeee
rs3c <rs3c@localhost.localdomain>
2023-04-06 18:34:29 -0600

    Writing HTML from Javascript

--- Practical/DOMmanipulation/script.js
+++ Practical/DOMmanipulation/script.js
@@ -10,3 +10,22 @@ console.log({
     pid,
     input
 });
+
+title.innerText = 'Dog <br>Cat</br>';
+
+// console.log(title.getAttribute('Tv'));
+// title.setAttribute('class', 'red');
+
+title.classList.add('red');
+title.classList.remove('red');
+
+input.value = "3899nmdfs9939"
+
+console.log(document.createElement('span'));
+
+const img = document.createElement('img');
+img.setAttribute('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCy_DSc1map-bY89X7DmI5lm7281JqguqETQ&usqp=CAU');
+console.log(img);
+
+pid.innerText = "";
+pid.append(img);
