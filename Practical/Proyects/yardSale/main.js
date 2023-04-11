14ac57f2a4fa3b1f15780501431af8702ca26921
rs3c <rs3c@localhost.localdomain>
2023-04-11 13:58:00 -0600

    Product details aside

--- Practical/Proyects/yardSale/main.js
+++ Practical/Proyects/yardSale/main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const iconCarMenu = document.querySelector('.navbar-shopping-cart');
 const iconMobileMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const shoppingCart = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -11,21 +11,21 @@ iconMobileMenu.addEventListener('click', toggleMobileMenu);
 iconCarMenu.addEventListener('click', toggleShoppingCart);
 
 function toggleDesktopMenu() {
-    const isShoppingCartClosed = shoppingCart.classList.contains('inactive');
+    const isShoppingCartClosed = shoppingCartContainer.classList.contains('inactive');
 
     if (!isShoppingCartClosed) {
-        shoppingCart.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
 
     desktopMenu.classList.toggle('inactive');
 };
 
 function toggleMobileMenu() {
-    const isShoppingCartClosed = shoppingCart.classList.contains('inactive');
+    const isShoppingCartClosed = shoppingCartContainer.classList.contains('inactive');
 
     if (!isShoppingCartClosed) {
         // if the shoppingCart it's open we have to close it to open the mobileMenu
-        shoppingCart.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
 
     mobileMenu.classList.toggle('inactive');
@@ -45,27 +45,27 @@ function toggleShoppingCart() {
         desktopMenu.classList.add('inactive');
     }
 
-    shoppingCart.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
 }
 
 const productList = [];
 productList.push({
-    name: 'Bike',
-    price: 120,
-    image: 'https://images.pexels.com/photos/34006/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    name: 'PC for Streamer imported from Africa Apple Somali Edition',
+    price: 21459,
+    image: 'https://i.pinimg.com/originals/4c/2b/cd/4c2bcd6faf26ec4e96299659f5cbdfda.jpg'
 })
 productList.push({
-    name: 'Old TV',
-    price: 1,
-    image: 'https://images.pexels.com/photos/333984/pexels-photo-333984.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    name: 'Consola Xbox Series X 1TB Ssd 4k Version Shrek Con Lector Discos',
+    price: 29703,
+    image: 'https://i.redd.it/8q59bzybfv871.jpg'
 })
 productList.push({
-  name: 'Pou 2 Digital Edition PS5'  ,
+  name: 'Pou 2 Edition PS5'  ,
   price: 10,
   image: 'https://assets.epuzzle.info/puzzle/075/910/original.jpg'
 })
 productList.push({
-    name: 'PlayStation 5 360MB SSD 13MB RAM',
+    name: 'PlayStation 360 SSD50MB 13MB RAM',
     price: 679,
     image: 'https://as01.epimg.net/meristation/imagenes/2014/03/31/reportaje/1396245600_573377_1532547880_sumario_normal.jpg'
 })
@@ -75,20 +75,32 @@ productList.push({
     image: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0d289213-d0be-4c42-b5b5-e54b479accc6/d5209o4-98653924-9701-4971-9da6-c69b469852c8.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzBkMjg5MjEzLWQwYmUtNGM0Mi1iNWI1LWU1NGI0NzlhY2NjNlwvZDUyMDlvNC05ODY1MzkyNC05NzAxLTQ5NzEtOWRhNi1jNjliNDY5ODUyYzguanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.NhRCa1TONF3_RA4Wyqyf3KQfxXul5VO-BczN4_BCwyM'
 })
 productList.push({
-    name: 'Pou 2 Premium Edition',
-    price: 80,
+    name: 'Pou 2 Xbox Premium Edition',
+    price: 83,
     image: 'https://preview.redd.it/0v0u8xcmmto61.jpg?width=592&format=pjpg&auto=webp&s=412dd896acab7330c823c0cf167b46f6bdae2016'
 })
 productList.push({
-    name: 'Pou 2 Edition PS4',
-    price: 7,
-    image: 'https://img.ifunny.co/images/5a5395fe6caf62b90776e1f1295600ec27c8d493ea5a79deb126fa67e5f643ec_1.jpg'
+    name: 'Twins Monkeys',
+    price: 79585,
+    image: 'https://images.pexels.com/photos/234054/pexels-photo-234054.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
 })
 productList.push({
-    name: 'PC for Streamer imported from Africa Apple Somali Edition',
-    price: 3459,
-    image: 'https://i.pinimg.com/originals/4c/2b/cd/4c2bcd6faf26ec4e96299659f5cbdfda.jpg'
+    name: 'Avengers Original Playera Marvel Utilizada Por Stan Lee',
+    price: 570.99,
+    image: 'https://purosarcasmo.com/wp-content/uploads/2021/04/Productos-20.jpg'
 })
+productList.push({
+    name: 'Backpack Shrek 4',
+    price: 34,
+    image: 'https://www.paredro.com/wp-content/uploads/2014/10/shrekhulk.jpg'
+})
+productList.push({
+    name: 'Backpack Harry Potter Obama Edition',
+    price: 422,
+    image: 'https://www.recreoviral.com/wp-content/uploads/2015/08/30-Marcas-piratas-de-los-chinos-5.jpg'
+})
+
+
 
 /*
 <div class="product-card">
@@ -119,7 +131,7 @@ for (product of productList) {
     const productInfoDiv = document.createElement('div');
     
     const productPrice = document.createElement('p');
-    productPrice.innerText = '$' + product.price + 'MXN'; 
+    productPrice.innerText = '$ ' + product.price + ' MXN'; 
     const productName = document.createElement('p');
     productName.innerText = product.name;
     
@@ -128,7 +140,7 @@ for (product of productList) {
 
     const productInfoFigure = document.createElement('figure');
     const productImgCard = document.createElement('img');
-    productImgCard.setAttribute('src', '/Icons/bt_add_to_cart.svg');
+    productImgCard.setAttribute('src', './Icons/bt_add_to_cart.svg');
     
     productInfoFigure.appendChild(productImgCard);
 
