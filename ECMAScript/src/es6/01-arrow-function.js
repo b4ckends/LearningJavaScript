03403d16cc82f033ea776b1398bc3ec4434e375d
rs3c <rs3c@localhost.localdomain>
2023-05-09 13:45:34 -0600

    Learned ECMAScript history and diferent versions of: let, const, arrow functions

---
+++ ECMAScript/src/es6/01-arrow-function.js
@@ -0,0 +1,25 @@
+const greet = (name) => console.log(`Hi ${name}`);
+greet("Cat");
+
+const greet2 = name => console.log(`Hi ${name}`);
+greet2('Dog');
+
+// const plus = function(a, b) {
+    // console.log(a + b);
+// };
+// plus(4, 2);
+
+const plus = (a, b) => console.log(a + b);
+plus(1, 1);
+
+// function square(num) {
+//     return num * num;
+// };
+
+// const square = (num) => {
+//     return num * num;
+// };
+
+const square = num => console.log(num * num);
+square(45);
+
