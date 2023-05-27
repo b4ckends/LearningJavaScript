8ea3e8010e3476f1e0eeaa5db8ae8b1188a15e25
rs3c <tartaacanon@gmail.com>
2023-05-26 20:32:46 -0600

    String padding

---
+++ ECMAScript/src/es8/02-string-padding.js
@@ -0,0 +1,11 @@
+const string = 'Hello';
+
+console.log(string.padStart(7, 'hi'));
+console.log(string.padStart(8, 'hi'));
+console.log(string.padStart(9, 'hi'));
+console.log(string.padStart(10, 'hi'));
+console.log(string.padStart(6, '_'));
+console.log(string.padEnd(6, '_'));
+
+
+
