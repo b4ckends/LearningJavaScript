8b84505f119ca05024c55c754c7428b3dd9947de
rs3c <tartaacanon@gmail.com>
2023-05-26 20:04:56 -0600

    Array includes

---
+++ ECMAScript/src/es7/02-array-includes.js
@@ -0,0 +1,9 @@
+let numbers = [1, 3, 4, 6, 7, 8, ];
+
+console.log(numbers.includes(8));
+
+const names = ['donald', 'dwayne the rock johnson', 'jennifer'];
+
+console.log(names.includes('dwayne the rock johnson'));
+
+console.log(names.includes('Donald'));
