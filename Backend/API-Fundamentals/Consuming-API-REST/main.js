bd614e2d38522063ce0f78996de7c8559a51718f
rs3c <tartaacanon@gmail.com>
2023-09-15 08:42:10 -0600

    Added button that generates random cat images

--- Backend/API-Fundamentals/Consuming-API-REST/main.js
+++ Backend/API-Fundamentals/Consuming-API-REST/main.js
@@ -2,9 +2,20 @@ console.log('Hello, world')
 
 const URL = 'https://api.thecatapi.com/v1/images/search';
 
-fetch(URL)
-  .then(res => res.json())
-  .then(data => {
-    const img = document.querySelector('img');
-    img.src = data[0].url;
-  })
+const btn = document.querySelector('#button');
+btn.addEventListener('click', alr);
+
+// This function calls the function called "ftc" which makes a fetch to show a random cat image
+function alr() {
+    ftc()
+}
+
+// This function makes a fetch for the random cat image of the API 
+function ftc() {
+    fetch(URL)
+      .then(res => res.json())
+      .then(data => {
+        const img = document.querySelector('img');
+        img.src = data[0].url;
+    });
+};
